5872ddc898cb0dd5f4c74b2283d0e2014f1221ac
khaledghazy <khaledghazy880@gmail.com>
2025-05-05 01:10:00 +0200

    createshoppingcartlist

---
+++ DepiProject/DepiProject/Controllers/CartController.cs
@@ -0,0 +1,40 @@
+﻿using DepiProject.Models;
+using DepiProject.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DepiProject.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            var shoppingcartvm = new ShoppingCartVM
+            {
+                ShoppingCart = new List<CartItem>
+    {
+        new CartItem
+        {
+            Id = 1,
+            Name = "Product 1",
+            Description = "Description of Product 1",
+            Price = 10.0,
+            Count = 2,
+            Image = "image1.jpg"
+        },
+        new CartItem
+        {
+            Id = 2,
+            Name = "Product 2",
+            Description = "Description of Product 2",
+            Price = 20.0,
+            Count = 1,
+            Image = "image2.jpg"
+        }
+    },
+                TotalPrice = 10.0 * 2 + 20.0 * 1 // = 40.0
+            };
+
+            return View(shoppingcartvm);
+        }
+    }
+}
---
+++ DepiProject/DepiProject/ViewModel/ShoppingCartVM.cs
@@ -0,0 +1,10 @@
+﻿using DepiProject.Models;
+
+namespace DepiProject.ViewModel
+{
+    public class ShoppingCartVM
+    {
+        public IEnumerable<CartItem> ? ShoppingCart { get; set; }
+        public double TotalPrice { get; set; }
+    }
+}
