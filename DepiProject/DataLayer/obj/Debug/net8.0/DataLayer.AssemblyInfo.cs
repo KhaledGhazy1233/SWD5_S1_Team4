e995b0acaa30b29ae1cff9087b9033289b3da258
Youssef-Ebn-Yasser <yh29152@gmail>
2025-05-13 19:52:47 +0300

    save

--- DepiProject/DataLayer/obj/Debug/net8.0/DataLayer.AssemblyInfo.cs
+++ DepiProject/DataLayer/obj/Debug/net8.0/DataLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c283dea23e822be0da7117a8f6f18bbd6d97a526")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38445f43ae5995e041f4c5e7c035ad3f379001d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
